f4ef104f4d468449c094e06d0e796bebc226fe7c
Christopher A. Taylor <chris@gameclosure.com>
2013-04-10 03:01:04 -0700

    Break it what am i doing

--- ImageLZWriter.cpp
+++ ImageLZWriter.cpp
@@ -13,13 +13,16 @@ void ImageLZWriter::clear() {
 		delete []_table;
 		_table = 0;
 	}
+	if (_visited) {
+		delete []_visited;
+		_visited = 0;
+	}
 
 	_rgba = 0;
 }
 
 static CAT_INLINE u32 hashPixel(u32 key) {
 	key <<= 8; // Remove alpha
-	key >>= 8;
 
 	// Thomas Wang's integer hash function
 	// http://www.cris.com/~Ttwang/tech/inthash.htm
@@ -47,6 +50,10 @@ bool ImageLZWriter::initWithRGBA(const u8 *rgba, int width, int height) {
 	_table = new u32[TABLE_SIZE];
 	memset(_table, 0xff, TABLE_SIZE * sizeof(u32));
 
+	const int visited_size = (width * height + 31) / 32;
+	_visited = new u32[visited_size];
+	memset(_visited, 0, visited_size * sizeof(u32));
+
 	for (int y = height - ZONE; y >= 0; y -= ZONE) {
 		for (int x = width - ZONE; x >= 0; x -= ZONE) {
 			u32 hash = 0;
@@ -65,7 +72,7 @@ bool ImageLZWriter::initWithRGBA(const u8 *rgba, int width, int height) {
 	return true;
 }
 
-static CAT_INLINE bool checkMatch(const u8 *rgba, int width, u16 x, u16 y, u16 mx, u16 my) {
+static bool checkMatch(const u8 *rgba, int width, u16 x, u16 y, u16 mx, u16 my) {
 	static const int ZONE = ImageLZWriter::ZONE;
 
 	for (int ii = 0; ii < ZONE; ++ii) {
@@ -83,6 +90,120 @@ static CAT_INLINE bool checkMatch(const u8 *rgba, int width, u16 x, u16 y, u16 m
 	return true;
 }
 
+static bool expandMatch(const u8 *rgba, int width, int height, u16 &sx, u16 &sy, u16 &dx, u16 &dy, u16 &w, u16 &h) {
+	static const int MAX_MATCH_SIZE = ImageLZWriter::MAX_MATCH_SIZE;
+
+	// Try expanding to the right
+	while (w + 1 <= MAX_MATCH_SIZE && sx + w < width && dx + w < width) {
+		for (int jj = 0; jj < h; ++jj) {
+			u32 *sp = (u32*)&rgba[((sx + w) + (sy + jj) * width)*4];
+			u32 *dp = (u32*)&rgba[((dx + w) + (dy + jj) * width)*4];
+
+			// If RGB components match,
+			if (((getLE(*sp) ^ getLE(*dp)) << 8) != 0) {
+				goto try_down;
+			}
+		}
+
+		// Widen width
+		++w;
+	}
+
+	// Try expanding down
+try_down:
+	while (h + 1 <= MAX_MATCH_SIZE && sy + h < height && dy + h < height) {
+		for (int jj = 0; jj < w; ++jj) {
+			u32 *sp = (u32*)&rgba[((sx + jj) + (sy + h) * width)*4];
+			u32 *dp = (u32*)&rgba[((dx + jj) + (dy + h) * width)*4];
+
+			// If RGB components match,
+			if (((getLE(*sp) ^ getLE(*dp)) << 8) != 0) {
+				goto try_left;
+			}
+		}
+
+		// Heighten height
+		++h;
+	}
+
+	// Try expanding left
+try_left:
+	while (w + 1 <= MAX_MATCH_SIZE && sx >= 1 && dx >= 1) {
+		for (int jj = 0; jj < h; ++jj) {
+			u32 *sp = (u32*)&rgba[((sx - 1) + (sy + jj) * width)*4];
+			u32 *dp = (u32*)&rgba[((dx - 1) + (dy + jj) * width)*4];
+
+			// If RGB components match,
+			if (((getLE(*sp) ^ getLE(*dp)) << 8) != 0) {
+				goto try_up;
+			}
+		}
+
+		// Widen width
+		sx--;
+		dx--;
+		++w;	
+	}
+
+	// Try expanding up
+try_up:
+	while (h + 1 <= MAX_MATCH_SIZE && sy >= 1 && dy >= 1) {
+		for (int jj = 0; jj < w; ++jj) {
+			u32 *sp = (u32*)&rgba[((sx + jj) + (sy - 1) * width)*4];
+			u32 *dp = (u32*)&rgba[((dx + jj) + (dy - 1) * width)*4];
+
+			// If RGB components match,
+			if (((getLE(*sp) ^ getLE(*dp)) << 8) != 0) {
+				return true;
+			}
+		}
+
+		// Widen width
+		sy--;
+		dy--;
+		++h;	
+	}
+
+	return true;
+}
+
+u32 ImageLZWriter::score(int x, int y, int w, int h) {
+	u32 sum = 0;
+
+	for (int ii = 0; ii < w; ++ii) {
+		for (int jj = 0; jj < h; ++jj) {
+			u32 *p = (u32*)&_rgba[((x + ii) + (y + jj) * _width)*4];
+			u32 pv = getLE(*p) << 8;
+			if (pv == 0) {
+				sum += 1 ^ visited(x + ii, y + jj);
+			} else {
+				sum += (1 ^ visited(x + ii, y + jj)) * ZERO_COEFF;
+			}
+		}
+	}
+
+	return sum / ZERO_COEFF;
+}
+
+void ImageLZWriter::add(int unused, u16 sx, u16 sy, u16 dx, u16 dy, u16 w, u16 h) {
+	// Mark pixels as visited
+	for (int ii = 0; ii < w; ++ii) {
+		for (int jj = 0; jj < h; ++jj) {
+			visit(dx + ii, dy + jj);
+		}
+	}
+
+	Match m = {
+		sx, sy, dx, dy, w - ZONE, h - ZONE
+	};
+
+	_matches.push_back(m);
+
+	cout << sx << "," << sy << " -> " << dx << "," << dy << " [" << w << "," << h << "] unused=" << unused << endl;
+
+	_covered += unused;
+}
+
 bool ImageLZWriter::match() {
 	if (!_rgba) {
 		return false;
@@ -91,8 +212,9 @@ bool ImageLZWriter::match() {
 	const u8 *rgba = _rgba;
 	const int width = _width;
 
-	u32 collisions = 0;
-	u32 matches = 0;
+	_collisions = 0;
+	_initial_matches = 0;
+	_covered = 0;
 
 	// For each raster,
 	for (u16 y = 0, yend = _height - ZONE; y <= yend; ++y) {
@@ -116,12 +238,35 @@ bool ImageLZWriter::match() {
 				if (mx != x && my != y) {
 
 					if (checkMatch(rgba, width, x, y, mx, my)) {
-						++matches;
-						if (mx != 0 || my != 0) {
-							cout << "Found hash match between (" << x << ", " << y << ") and (" << mx << ", " << my << ")" << endl;
+						++_initial_matches;
+
+						// Determine source and destination in decoder order
+						u16 sx = x, sy = y, dx = mx, dy = my, w = ZONE, h = ZONE;
+						if (my > y) {
+						} else if (my == y) {
+							if (mx > x) {
+							} else {
+								sx = mx;
+								sy = my;
+								dx = x;
+								dy = y;
+							}
+						} else {
+							sx = mx;
+							sy = my;
+							dx = x;
+							dy = y;
+						}
+
+						// See how far the match can be expanded
+						expandMatch(rgba, width, _height, sx, sy, dx, dy, w, h);
+
+						int unused = score(dx, dy, w, h);
+						if (unused >= MIN_SCORE) {
+							add(unused, sx, sy, dx, dy, w, h);
 						}
 					} else {
-						++collisions;
+						++_collisions;
 					}
 				}
 			}
@@ -135,8 +280,10 @@ bool ImageLZWriter::match() {
 		} while (++x <= xend);
 	}
 
-	cout << matches << " matches" << endl;
-	cout << collisions << " collisions" << endl;
+	cout << _initial_matches << " initial matches" << endl;
+	cout << _collisions << " initial collisions" << endl;
+	cout << _covered << " covered / " << width * _height << endl;
+	cout << _matches.size() * 10 << " bytes overhead / " << _covered * 3 << " bytes saved" << endl;
 
 	return true;
 }
--- ImageLZWriter.hpp
+++ ImageLZWriter.hpp
@@ -12,7 +12,7 @@
  * It finds repeated blocks of pixels in the original RGB raster so that later
  * instances of those blocks can be encoded.
  *
- * First step is to scan the whole image in 8x8 blocks and hash each block into
+ * First step is to scan the whole image in 4x4 blocks and hash each block into
  * a big hash table.  The index of the hash table is the 32-bit hash number and
  * the value at each index is the location to find that block.  The scan is
  * done from the lower right to the upper left so that the hash table prefers
@@ -22,12 +22,13 @@
  * increment at a time.  Each image match is verified and then expanded.
  * Forward matches are as useful as backwards matches at this point.
  *
- * To avoid overlaps, a simple algorithm is used:
- *
- * When a match is found, the one farther right/down locks the 8x8 blocks that
- * are completely covered, and those cannot be used again for further matches.
+ * To avoid overlaps, a simple algorithm is used: Each pixel for the match
+ * copy destination is flagged as being used, and cannot be used as part of
+ * another match.  New matches must have at least 16 previously unused pixels.
  *
  * To avoid slowing down too much, ~256x256 is the largest match allowed.
+ * Compression speed is definitely not any consideration however, not that it
+ * runs terribly slowly.
  *
  * The result is a set of pixel source/dest x,y coordinates (32+32 bits) and a
  * width/height (8+8 bits) or 10 bytes of overhead.  These are transmitted with
@@ -39,11 +40,14 @@ namespace cat {
 
 class ImageLZWriter {
 public:
-	static const int ZONE = 8;
-	static const int TABLE_BITS = 16;
+	static const int ZONE = 4;
+	static const int TABLE_BITS = 17;
 	static const int TABLE_SIZE = 1 << TABLE_BITS;
 	static const u32 TABLE_MASK = TABLE_SIZE - 1;
 	static const u32 TABLE_NULL = 0xffffffff;
+	static const int MAX_MATCH_SIZE = 255 + ZONE;
+	static const int MIN_SCORE = 16;
+	static const int ZERO_COEFF = 4; // Count zeroes as being worth 1/4 of a normal match
 
 protected:
 	const u8 *_rgba;
@@ -53,6 +57,18 @@ protected:
 	u32 *_table;
 	int _table_size;
 
+	// Visited bitmask
+	u32 *_visited;
+
+	CAT_INLINE void visit(int x, int y) {
+		int off = x + y * _width;
+		_visited[off >> 5] |= 1 << (off & 31);
+	}
+	CAT_INLINE u32 visited(int x, int y) {
+		int off = x + y * _width;
+		return (_visited[off >> 5] >> (off & 31)) & 1;
+	}
+
 	struct Match {
 		u16 sx, sy;
 		u16 dx, dy;
@@ -60,13 +76,17 @@ protected:
 	};
 
 	std::vector<Match> _matches;
+	u32 _covered, _collisions, _initial_matches;
 
 	void clear();
+	u32 score(int x, int y, int w, int h);
+	void add(int unused, u16 sx, u16 sy, u16 dx, u16 dy, u16 w, u16 h);
 
 public:
 	CAT_INLINE ImageLZWriter() {
 		_rgba = 0;
 		_table = 0;
+		_visited = 0;
 	}
 	virtual CAT_INLINE ~ImageLZWriter() {
 		clear();
