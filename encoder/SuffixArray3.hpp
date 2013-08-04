1da88fb5b0f6038419ab2ac4f3a8abfd0c234535
Chris A. Taylor <chris@gameclosure.com>
2013-08-03 18:21:08 -0700

    Add more comments

--- encoder/SuffixArray3.hpp
+++ encoder/SuffixArray3.hpp
@@ -11,6 +11,24 @@
  * This is useful for finding the longest LZ match within a 1M window at each
  * byte, which is then used for optimal LZ parsing.
  * See LZMatchFinder.hpp for more information about how we use it.
+ *
+ * There are a few small challenges to adopt this approach:
+ *
+ * (1) For RGBA it sometimes finds unaligned matches (since it's byte-wise)
+ * that are not useful for compression.  We currently just discard these and
+ * fall back to the bounded hash chain approach.  There is no way to change SA3
+ * to return word matches without a lot of work.
+ *
+ * (2) We don't really want the longest match, since the savings for each pixel
+ * are not equal.  Ssome pixels take more bits to represent than others.  The
+ * cost to represent distances varies by up to 16 bits.  So we modified the SA3
+ * algorithm to return two matches and we consider both as well as local
+ * matches found using a simple hash chain approach, which returns nearby
+ * matches first.  Nearby matches often have a short distance encoding and can
+ * be preferable over the long-distance longest SA3 match.
+ *
+ * Combining both hash chains and suffix-array based matchers turned out to
+ * yield an algorithm that is both fast and compresses well.
  */
 
 namespace cat {
@@ -21,7 +39,6 @@ namespace cat {
 		// sortLookup[i] gives you the sort order of file position i
 		// sortIndex[sortLookup[i]] == i
 		// sortSameLen[i] gives you the pairwise match len of {i} and {i+1} in the sort order
-
 		std::vector<int> sortIndex;
 		std::vector<int> sortSameLen;
 		std::vector<int> sortIndexInverse;
